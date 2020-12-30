4dab37a2082b8e4dd1cfc9cb5ee8169a16fd2536
shaiksaajid11 <shaiksaajid@msitprogram.net>
2020-12-30 19:57:29 +0530

    assignment4 done

---
+++ Data Mining/DM Assignment4/7th.R
@@ -0,0 +1,17 @@
+setwd("D:\\DataScience_2019501071\\Data Mining\\DM Assignment4")
+install.packages("class")
+library(class)
+
+train<-read.csv("sonar_train.csv",header=FALSE)
+test <- read.csv("sonar_test.csv",header=FALSE)
+x_train=train[,1:60]
+y_train=as.factor(train[,61])
+x_test<-test[,1:60]
+y_test<-as.factor(test[,61])
+
+help("knn")
+model1<-knn(x_train,x_test,y_train,k=5)
+1-sum(y_test==model1)/length(y_test)
+
+model2<-knn(x_train,x_test,y_train,k=6)
+1-sum(y_test==model2)/length(y_test)
